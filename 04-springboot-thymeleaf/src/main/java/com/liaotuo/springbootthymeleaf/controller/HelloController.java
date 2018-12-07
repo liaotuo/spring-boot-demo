4985aa06024392798028d232c07ca589a339ea1d
liaotuo <1481118216@qq.com>
2018-12-07 17:48:17 +0800

    springboot 模板引擎thymeleaf

---
+++ 04-springboot-thymeleaf/src/main/java/com/liaotuo/springbootthymeleaf/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.liaotuo.springbootthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 18070959
+ * @create 2018-12-07 5:20 PM
+ * @desc
+ **/
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(ModelMap map){
+        // 添加测试属性
+        map.addAttribute("github", "https://github.com/liaotuo");
+        // 返回字符串 为对应模板文件名 example: src/main/resources/templates/index.html
+        return "index";
+    }
+}
