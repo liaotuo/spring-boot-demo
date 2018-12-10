ca7f170fb130787fd8b252eb070872261d71ec7d
liaotuo <1481118216@qq.com>
2018-12-10 21:09:14 +0800

    Spring Boot中使用Spring-data-jpa让数据访问更简单、更优雅

---
+++ 11-springboot-jpa/src/main/java/com/liaotuo/springboot/repo/UserRepository.java
@@ -0,0 +1,20 @@
+package com.liaotuo.springboot.repo;
+
+import com.liaotuo.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * @author 18070959
+ * @create 2018-12-10 4:34 PM
+ * @desc
+ **/
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+
+    User findByNameAndAge(String name, Integer age);
+
+    @Query("from User u where u.name=:name")
+    User findUser(@Param("name") String name);
+}
