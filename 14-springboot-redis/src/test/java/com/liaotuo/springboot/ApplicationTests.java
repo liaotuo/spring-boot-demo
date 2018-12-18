c798d30a359f0ccaa9060801eb03d47749eaa1d7
liaotuo <1481118216@qq.com>
2018-12-18 17:05:44 +0800

    springboot2.0 使用redis

---
+++ 14-springboot-redis/src/test/java/com/liaotuo/springboot/ApplicationTests.java
@@ -0,0 +1,23 @@
+package com.liaotuo.springboot;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void test() {
+        // 保存字符串
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# spring-boot-demo
+# 目录
 springboot 学习demo
+[TOC]
+
+# 13-springboot-redis
+## 参考
+- [Spring Boot中使用Redis数据库 | 程序猿DD](http://blog.didispace.com/springbootredis/)
+- [Redis](https://redis.io/)
+- [Redis学习总结之三——Redis客户端命令 - Emberd - 博客园](https://www.cnblogs.com/emberd/p/4210218.html)
+## 问题
+- [redis - 第三节:保护模式配置 | hans's Blog](https://hans007.github.io/redis/2016/12/07/redis-protected-03)
+- [【解决】io.lettuce.core.RedisCommandTimeoutException: Command timed out - 东天里的冬天 - CSDN博客](https://blog.csdn.net/gwd1154978352/article/details/79630245)
+
