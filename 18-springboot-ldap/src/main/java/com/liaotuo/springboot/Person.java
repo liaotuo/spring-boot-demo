b7f99e6e0e041816f7c2e348e67151c194fb5edf
liaotuo <1481118216@qq.com>
2019-03-20 19:53:57 +0800

    springboot使用ldap做统一认证（未完成）

--- 18-springboot-ldap/src/main/java/com/liaotuo/springboot/Person.java
+++ 18-springboot-ldap/src/main/java/com/liaotuo/springboot/Person.java
@@ -1,9 +1,25 @@
 package com.liaotuo.springboot;
 
+import org.springframework.ldap.odm.annotations.Attribute;
+import org.springframework.ldap.odm.annotations.DnAttribute;
+import org.springframework.ldap.odm.annotations.Entry;
+import org.springframework.ldap.odm.annotations.Id;
+
 /**
  * @author 18070959
  * @date 2019-03-20 11:41 AM
  * @description
  **/
+@Entry(base = "ou=people,dc=liaotuo,dc=com", objectClasses = "inetOrgPerson")
 public class Person {
+    @Id
+    private Integer id;
+    @DnAttribute(value = "uid", index = 3)
+    private String uid;
+    @Attribute(name = "cn")
+    private String commonName;
+    @Attribute(name = "sn")
+    private String suerName;
+    private String userPassword;
+
 }
